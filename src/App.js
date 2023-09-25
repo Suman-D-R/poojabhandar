f404d22c86b1f13516f355abf26ff83585c03a2c
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-09-26 00:02:24 +0530

    pic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import n from './1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>gudli pooje for poojabhandar by suhas varun and suman</h1>
+      <img src={n} />
     </div>
   );
 }
